e0d58e3b5caa8dea10edf6d91369ecc44091efd8
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-04-19 20:42:11 +0900

    Create codekata_47.sql
    SQL 코드카타 47번 문제

---
+++ codekata_47.sql
@@ -0,0 +1,10 @@
+# 모든 레코드 조회하기 
+# https://school.programmers.co.kr/learn/courses/30/lessons/59034 
+# ??? : 문제가 너무 쉬운데?
+
+SELECT *
+from animal_ins 
+order by animal_id 
+
+# 개인 문제 풀이 과정 
+# https://kitsch2023.tistory.com/196 
