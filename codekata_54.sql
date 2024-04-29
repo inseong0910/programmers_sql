c6a82e983d9fdaa1b17c27198f12870c8ce11bee
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-04-29 18:54:37 +0900

    Create codekata_54.sql
    SQL 코드카타 54번 문제

---
+++ codekata_54.sql
@@ -0,0 +1,9 @@
+# 최댓값 구하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/59415
+
+SELECT MAX(DATETIME) AS 시간
+FROM ANIMAL_INS;
+
+# 주요 개념 : 집계함수 max() 
+# 개인 문제 풀이
+# https://kitsch2023.tistory.com/215 
